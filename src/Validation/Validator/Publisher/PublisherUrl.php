ce4e62da37d5f62d74f6539bd716da0bd6efd0cb
Stelmaszv <stelmaszv@gmial.com>
2024-04-09 12:43:35 +0200

    clean code

--- src/Validation/Validator/Publisher/PublisherUrl.php
+++ src/Validation/Validator/Publisher/PublisherUrl.php
@@ -11,5 +11,5 @@ use Symfony\Component\Validator\Constraint;
  */
 class PublisherUrl extends Constraint
 {
-    public $message = 'Host {{ host }} jest not allowed.';
+    public $message = 'Host {{ host }} is not allowed.';
 }
