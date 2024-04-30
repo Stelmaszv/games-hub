ba465e79b7de9dcb2075955e3ffa10f9c5382dc0
Stelmaszv <stelmaszv@gmial.com>
2024-04-30 13:10:08 +0200

    rool back Publisher

--- src/Controller/Publisher/ListPublisherController.php
+++ src/Controller/Publisher/ListPublisherController.php
@@ -3,14 +3,18 @@
 declare(strict_types=1);
 
 namespace App\Controller\Publisher;
-use App\Entity\Publisher;
-use Symfony\Component\Routing\Annotation\Route;
+
 use App\Generic\Api\Controllers\GenericListController;
+use App\Entity\Publisher;
+use App\Security\Atribute;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("api/publisher/list", name: "publishers_list", methods: ["GET"])]
 class ListPublisherController extends GenericListController
 {
     protected ?string $entity = Publisher::class;
+
+    protected ?string $voterAtribute = Atribute::CAN_LIST_PUBLISHERS;
 
     protected function onQuerySet(): array
     {
--- src/Controller/Publisher/ShowPublisherController.php
+++ src/Controller/Publisher/ShowPublisherController.php
@@ -3,13 +3,17 @@
 declare(strict_types=1);
 
 namespace App\Controller\Publisher;
-use App\Entity\Publisher;
-use Symfony\Component\Routing\Annotation\Route;
+
 use App\Generic\Api\Controllers\GenericDetailController;
+use App\Entity\Publisher;
+use App\Security\Atribute;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("api/publisher/show/{id}", name: "publisher_show", methods: ["GET"])]
 class ShowPublisherController extends GenericDetailController
 {
     protected ?string $entity = Publisher::class;
+    protected ?string $voterAtribute = Atribute::CAN_SHOW_PUBLISHER;
+    protected ?string $voterSubject = Publisher::class;
 
 }
