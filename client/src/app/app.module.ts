eb2ff99e22dbac50db613db750f1256a63b5ae38
Stelmaszv <stelmaszv@gmial.com>
2024-05-15 14:35:18 +0200

    clean code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,17 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
-import { TranslatePipe } from './pipe/common/TranslatePipe'
+import { TranslationService } from './services/translation/translation.service';
 
 import { AppComponent } from './app.component';
 import { PublishersComponent } from './components/sections/publishers/publishers.component';
 import { NavbarComponent } from './components/sections/common/navbar/navbar.component';
 import { UserNavbarComponent } from './components/sections/common/navbar/user-navbar/user-navbar.component';
 import { UserProfilModalComponent } from './components/sections/common/navbar/user-navbar/user-profil-modal/user-profil-modal.component';
-
-import { TranslationService } from './services/translation/translation.service';
-
-import { registerLocaleData } from '@angular/common';
-import localePl from '@angular/common/locales/pl';
 import { MessagegesModelComponent } from './components/sections/common/navbar/user-navbar/messageges-model/messageges-model.component';
 import { NotificationModelComponent } from './components/sections/common/navbar/user-navbar/notification-model/notification-model.component';
-
-registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
@@ -27,7 +20,6 @@ registerLocaleData(localePl);
     NavbarComponent,
     UserNavbarComponent,
     UserProfilModalComponent,
-    TranslatePipe,
     MessagegesModelComponent,
     NotificationModelComponent
   ],
@@ -36,7 +28,7 @@ registerLocaleData(localePl);
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pl' },TranslationService],
+  providers: [TranslationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
