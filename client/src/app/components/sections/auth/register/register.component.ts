a4f01535b4adf13d07e6c59ce3cee3f249cb867b
Stelmaszv <stelmaszv@gmial.com>
2024-05-22 14:40:39 +0200

    add hide password

--- client/src/app/components/sections/auth/register/register.component.ts
+++ client/src/app/components/sections/auth/register/register.component.ts
@@ -12,6 +12,8 @@ import { FormValidatorService } from 'src/app/services/common/form-validator/for
 })
 export class RegisterComponent {
 
+  public showPasswordIcon :string = 'fa-solid fa-eye' // fa-sharp fa-solid fa-eye-slash
+  public showPasswordRepartIcon :string = 'fa-solid fa-eye' // fa-sharp fa-solid fa-eye-slash
   public errorMessage: string = '';
   public email: string | null = '';
   public password: string | null = '';
@@ -24,6 +26,30 @@ export class RegisterComponent {
     private authService : AuthService,
     private formValidatorService: FormValidatorService
   ) {}
+
+  showPassword(): void {
+    const inputElement = document.querySelector<HTMLInputElement>('#password');
+  
+    if (!inputElement) {
+      console.error("Input element '#password' not found.");
+      return;
+    }
+  
+    this.showPasswordIcon = (inputElement.type === 'password') ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+    inputElement.type = (inputElement.type === 'password') ? 'text' : 'password';
+  }
+  
+  showRepartPassword(): void {
+    const inputElement = document.querySelector<HTMLInputElement>('#repartPassword');
+  
+    if (!inputElement) {
+      console.error("Input element '#repartPassword' not found.");
+      return;
+    }
+  
+    this.showPasswordRepartIcon = (inputElement.type === 'password') ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+    inputElement.type = (inputElement.type === 'password') ? 'text' : 'password';
+  }
 
   onSubmit() {
     this.httpServiceService.postData('http://localhost/api/register',{ 
