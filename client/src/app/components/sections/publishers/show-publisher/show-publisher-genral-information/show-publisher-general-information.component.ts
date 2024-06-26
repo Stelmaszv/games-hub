7674f90f61a3d2eb0d4f0a677a45acae3836b521
Stelmaszv <stelmaszv@gmial.com>
2024-06-26 11:55:38 +0200

    clean code

--- client/src/app/components/sections/publishers/show-publisher/show-publisher-genral-information/show-publisher-general-information.component.ts
+++ client/src/app/components/sections/publishers/show-publisher/show-publisher-genral-information/show-publisher-general-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Publisher } from '../../interfaces';
 import { TranslationService } from 'src/app/services/common/translation/translation.service';
 
@@ -7,18 +7,13 @@ import { TranslationService } from 'src/app/services/common/translation/translat
   templateUrl: './show-publisher-general-information.component.html',
   styleUrls: ['./show-publisher-general-information.component.scss']
 })
-export class ShowPublisherGeneralInformationComponent implements OnInit {
+export class ShowPublisherGeneralInformationComponent{
   @Input() publisher!: Publisher|null;
   @Input() edit!: boolean | undefined;
 
   constructor(public translationService: TranslationService){}
 
-  public ngOnInit(): void {
-    console.log(this.publisher)
-  }
-
   onPublisherChange(updatedPublisher: Publisher): void {
-    console.log(updatedPublisher)
     this.publisher = updatedPublisher;
   }
 }
