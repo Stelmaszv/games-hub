0419cd2fc2b83a6333aa39b947835c154ef997b3
Stelmaszv <stelmaszv@gmial.com>
2024-07-09 17:34:49 +0200

    fix build issue

--- client/src/app/components/sections/publishers/show-publisher/show-publisher-genral-information/show-publisher-general-information.component.ts
+++ client/src/app/components/sections/publishers/show-publisher/show-publisher-genral-information/show-publisher-general-information.component.ts
@@ -13,7 +13,4 @@ export class ShowPublisherGeneralInformationComponent{
 
   constructor(public translationService: TranslationService){}
 
-  public onPublisherChange(updatedPublisher: Publisher): void {
-    this.publisher = updatedPublisher;
-  }
 }
